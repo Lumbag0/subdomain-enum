9a05e40c16acdfdce34040395433c24f16b1d2a9
lumbago <ecape0207@gmail.com>
2024-12-20 20:15:01 -0500

    Added input checking along with error checking for installing jq

--- subdomain-enum.sh
+++ subdomain-enum.sh
@@ -8,29 +8,74 @@ function banner {
     echo "|--------Subdomain-Enum--------|"
     echo "================================"
     echo "|Author: Lumbag0               |"
-    echo -e "================================\n"
+    echo "================================"
+}
+
+# Description: Validates the required input to the script
+# Paremters: status of options_used, url_set, out_set
+# Returns: 1 if any one of the required inputs is not passed, 0 if successful
+function validate_input {
+    local options_used=$1
+    local url_set=$2
+    local out_set=$3
+    local can_continue=0
+
+    # Check that an argument was passed
+    if ((options_used == 1)); then
+        echo "ERROR: No Arguments Passed to the script, quitting..." > /dev/stderr
+        usage
+        can_continue=1
+        return $can_continue
+    fi
+    
+    # Check that the target url was passed
+    if ((url_set == 1)); then
+        echo "ERROR: Target not specified, quitting..." > /dev/stderr
+        usage
+        can_continue=1
+        return $can_continue
+    fi 
+
+    # Check that the output file was passed
+    if ((out_set == 1)); then
+        echo "ERROR: Output file not specified, quitting..." > /dev/stderr
+        usage
+        can_continue=1
+        return $can_continue
+    fi 
+
+    return $can_continue
 }
 
 # Description: Checks to make sure necessary commands are installed
 # Parameters: N\A
-# Returns: N\A
+# Returns: 0 if successfully installed jq or if jq is already installed 1 if install fails
 function check_dependencies {
     # Check that jq is installed
+    installed_jq=0
     if ! which jq > /dev/null; then 
-        sudo apt update && sudo apt install jq
+        if ! sudo apt update && sudo apt install jq; then
+            echo "ERROR: Could not install jq, quitting..." > /dev/stderr
+            installed_jq=1
+            return $installed_jq
+        else
+            return $installed_jq
+        fi
     fi
+    return $installed_jq
 }
 
 # Description: Prints usage information to the terminal
 # Parameters: N\A
 # Returns: N\A
 function usage {
-    echo "OPTIONS:"
+    echo "REQUIRED OPTIONS:"
     echo "-u <DOMAIN>: Domain to query for"
-    echo "-h: Display this help menu"
     echo "-o: Output to file"
-    echo "USAGE: ./subdomain-enum.sh -u <DOMAIN>"
-    echo "EXAMPLE: ./subdomain-enum.sh -u google.com"
+    echo -e "\nOPTIONAL OPTIONS:"
+    echo "-h: Display this help menu"
+    echo -e "\nUSAGE: ./subdomain-enum.sh -u <DOMAIN> -o <OUTPUT_FILE>"
+    echo "EXAMPLE: ./subdomain-enum.sh -u google.com -o google_domains"
 }
 
 # Description: Checks that crt.sh is reachable
@@ -72,22 +117,20 @@ function crt_search {
     local domain=$1
     local output_file=$2
 
-    # If output file is not passed, then set the name of the domains file to the name of the domain being searched
-    if [[ -z $output_file ]]; then
-        curl -s "https://crt.sh/?q=$domain&output=json" | jq -r '.[].common_name' | sort -u | tee "$domain"
-    else
-        curl -s "https://crt.sh/?q=$domain&output=json" | jq -r '.[].common_name' | sort -u | tee "$output_file"
-    fi
+    curl -s "https://crt.sh/?q=$domain&output=json" -o "$domain"_raw_json
+    jq -r '.[].common_name' < "$domain"_raw_json | sort -u | tee "$domain"
 }
 
 domain=
-output_file=
+output_file=1
+url_set=1
+out_set=1
 
 # Print Banner
 banner
 
 while getopts ":u:ho:" option; do
-    options_used=1
+    options_used=0
     case $option in 
         h) 
             usage
@@ -95,9 +138,11 @@ while getopts ":u:ho:" option; do
             ;;
         u)
             domain=$OPTARG
+            url_set=0
             ;;
         o)
             output_file=$OPTARG
+            out_set=0
             ;;
         \?)
             echo "[*] ERROR: Invalid Option: $OPTARG" > /dev/stderr
@@ -106,10 +151,13 @@ while getopts ":u:ho:" option; do
     esac
 done
 
-# Checn to make sure that options were passed
-if ((options_used == 0)); then
-    echo "[*] ERROR: NO OPTIONS PASSED" > /dev/stderr
-    usage
+# Validate that arguments, url, and the output file are set, if one of them is not, then exit
+if ! validate_input "$options_used" "$url_set" "$out_set"; then
+    exit 1
+fi
+
+# Make sure neccessary packages are installed, if installation fails, exit
+if ! check_dependencies; then
     exit 1
 fi
 
@@ -117,9 +165,6 @@ fi
 if ! crt_reachable; then
     exit 1
 fi
-
-# Make sure neccessary packages are installed
-check_dependencies
 
 # Clean input
 domain=$(clean_input "$domain")
